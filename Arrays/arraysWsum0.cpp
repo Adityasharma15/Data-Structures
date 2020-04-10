9ea5e77d8af5134ac654fc2dd173c64a3a553680
Aditya <aditya.sharma5645@gmail.com>
2020-04-10 14:17:00 +0530

    Arrays with sum equal to 0 or k

---
+++ Arrays/arraysWsum0.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+  cin.tie(0);
+	cout.tie(0);
+
+	ll t;
+	cin >> t;
+
+	while(t--)
+  {
+
+    ll n;
+    cin >> n;
+
+    unordered_map<ll,ll> um;
+    um[0]++;
+
+    ll x, sum = 0, ans = 0;
+
+    for(ll i = 0;i<n; i++)
+    {
+        cin >> x;
+        sum += x;
+
+        if(um.find(sum)!=um.end())
+        {
+          ans+= um[sum];
+        }
+
+        um[sum]++;
+    }
+
+      cout << ans << "\n";
+	}
+
+  return 0;
+}
