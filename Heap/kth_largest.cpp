a53eeb7671cc960a90fcf1c68176510ffb0e2dc6
Aditya <aditya.sharma5645@gmail.com>
2020-08-01 20:58:58 +0530

    Kth Largest Element in an Array

--- Heap/kth_largest.cpp
+++ Heap/kth_largest.cpp
@@ -1,64 +1,18 @@
-//kth_largest.cpp
+// 215. Kth Largest Element in an Array
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-#include<bits/stdc++.h>
-#define ll long long
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
 
-using namespace std;
+        priority_queue<int, vector<int> , greater<int> > min_heap;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            min_heap.push(nums[i]);
 
-class Minheap
-{
-	ll size;
-	ll capacity;
-	ll * heap;
-
-	void Minheap(int capac);
-
-	void insert(int i);
-
-	int extractMin();
-
-}
-
-void Minheap::Minheap(int capac)
-{
-	ll * head = new int[capac];
-	size = 0;
-	capacity = capac;
-}
-
-void Minheap::insert(int i)
-{
-	if(i <= heap[0])
-		return;
-
-	
-}
-
-
-
-
-
-
-
-int main()
-{
-	ll t;
-	cin >> t;
-
-	while(t--)
-	{
-		ll k, n;
-		cin >> k >> n;
-
-		for(ll i = 0; i<n; i++)
-		{
-			cin >> 
-		}
-
-
-
-	}
-
-
-
-}
+            if(min_heap.size()>k)
+                    min_heap.pop();
+        }
+        return min_heap.top();
+    }
+};
