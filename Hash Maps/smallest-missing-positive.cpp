5ef3b6205f3510aebd4d498d444cb0360c4efa32
Aditya <aditya.sharma5645@gmail.com>
2020-08-02 00:17:02 +0530

    Smallest Positive missing number

---
+++ Hash Maps/smallest-missing-positive.cpp
@@ -0,0 +1,62 @@
+// Time complexity - O(n) & Space complexity - O(1).
+// Smallest Positive missing number
+// https://www.geeksforgeeks.org/find-the-smallest-positive-number-missing-from-an-unsorted-array/
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+  cin.tie(0);
+	cout.tie(0);
+
+	ll t;
+	cin >> t;
+
+	while(t--)
+  {
+    ll n;
+    cin >> n;
+
+    vector<ll> arr;
+    ll maxx = 0;
+    for(ll i = 0; i<n; i++)
+    {
+        ll temp;
+        cin >> temp;
+
+        if(temp>0)
+        arr.push_back(temp);
+
+        maxx = max(maxx, temp);
+    }
+
+    ll sizee = arr.size();
+
+    for(ll i = sizee; i<=maxx; i++)
+      arr.push_back(maxx);
+
+    for(ll i = 0; i<arr.size(); i++)
+    {
+        ll temp = arr[i];
+
+        if(temp<0)
+          temp *= -1;
+
+        if(arr[temp-1] >0)
+        arr[temp-1] *= -1;
+    }
+
+    ll i=1;
+    for( i = 0; i<arr.size(); i++)
+    {
+      if(arr[i]>=0)
+        break;
+    }
+
+    cout << i+1 << "\n";
+	}
+  return 0;
+}
