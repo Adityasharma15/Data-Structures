40d8a99301eb3f27c0f08fcb6d5d1c13c45f0769
Aditya <aditya.sharma5645@gmail.com>
2020-08-20 16:12:02 +0530

    Merge Two Sorted Lists

---
+++ Linked List/merge-two-sorted-llists.cpp
@@ -0,0 +1,24 @@
+// 21. Merge Two Sorted Lists
+
+class Solution {
+
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+         if(!l1 || !l2)
+            return !l1? l2 : l1;
+
+          if(l1->val <= l2->val)
+          {
+              l1 ->next = mergeTwoLists(l1->next, l2);
+              return l1;
+          }
+
+           else
+           {
+             l2->next = mergeTwoLists(l1, l2->next);
+             return l2;
+           }
+    }
+
+};
