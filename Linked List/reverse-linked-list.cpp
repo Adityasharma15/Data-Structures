9fd1df2c0423dc6fc1c1e20fe5d139d38e2fdcfe
Aditya <aditya.sharma5645@gmail.com>
2020-10-02 17:49:01 +0530

    added some comments to reverse LL

--- Linked List/reverse-linked-list.cpp
+++ Linked List/reverse-linked-list.cpp
@@ -8,10 +8,16 @@ public:
             ListNode * curr = head;
             ListNode * prev = NULL;
 
-            while(head!=NULL)
-            {
+            while(head!=NULL){
+            // head moves to the next node in the list because we will
+            // change the next of the node head is pointing currently to the
+            // previous node of this list.
                 curr = head;
                 head = head->next;
+                
+            // we change the next of this node we are visiting, head is alread
+            // pointing to the next node of this list that needs to be 
+            // reversed.     
                 curr->next = prev;
                 prev = curr;
             }
